4775e2c26db52c1a01aa07f3a0eb418d5b7560b2
Steve Baer <steve@mcneel.com>
2025-11-11 14:06:30 -0800

    almost there

--- main.cpp
+++ main.cpp
@@ -15,6 +15,9 @@ int main(int argc, char** argv)
     Slang::ComPtr<slang::IGlobalSession> globalSession;
 
     HMODULE hSlang = LoadLibrary("slang.dll");
+    if (0 == hSlang)
+      return 1;
+
     CREATEGLOBALSESSIONPROC SlangStart = (CREATEGLOBALSESSIONPROC)GetProcAddress(hSlang, "slang_createGlobalSession");
 
     slang::IGlobalSession** gs = globalSession.writeRef();
@@ -24,7 +27,7 @@ int main(int argc, char** argv)
     globalSession->createSession(sessionDesc, session.writeRef());
 
     slang::TargetDesc targetDesc = {};
-    targetDesc.format = SLANG_DXBC; // FXC-compatible .cso
+    targetDesc.format = SlangCompileTarget::SLANG_DXBC; // FXC-compatible .cso
     targetDesc.profile = globalSession->findProfile("ps_5_0");
     targetDesc.flags = 0;
 
@@ -32,27 +35,23 @@ int main(int argc, char** argv)
     globalSession->createCompileRequest(&request);
 
     int unit = request->addTranslationUnit(SLANG_SOURCE_LANGUAGE_SLANG, nullptr);
-
-    /*
-    slang_addSourceFile(request, unit, inputPath);
-    slang_setTarget(request, 0, SLANG_BINARY_FORMAT_DXBC);
-    slang_setTargetProfile(request, 0, targetDesc.profile);
-    slang_setEntryPoint(request, unit, "main", SLANG_STAGE_FRAGMENT);
-
-    if (slang_compile(request) != SLANG_OK)
+//    slang_addSourceFile(request, unit, inputPath);
+    request->setCodeGenTarget(SlangCompileTarget::SLANG_DXBC);
+    request->setTargetProfile(0, targetDesc.profile);
+    request->addEntryPoint(unit, "shader_main", SlangStage::SLANG_STAGE_FRAGMENT);
+    SlangResult compileResult = request->compile();
+    if (compileResult != SLANG_OK)
     {
-        std::cerr << "Slang compilation failed:\n";
-        std::cerr << slang_getDiagnosticOutput(request) << "\n";
-        return 1;
+      std::cerr << "Slang compilation failed:\n";
+      std::cerr << request->getDiagnosticOutput() << "\n";
+      return 1;
     }
 
     size_t blobSize = 0;
-    const void* blob = slang_getEntryPointCode(request, 0, 0, &blobSize);
-
-    std::ofstream out(outputPath, std::ios::binary);
-    out.write(reinterpret_cast<const char*>(blob), blobSize);
-    out.close();
-*/
+    const void* blob = request->getEntryPointCode(0, &blobSize);
+    //std::ofstream out(outputPath, std::ios::binary);
+    //out.write(reinterpret_cast<const char*>(blob), blobSize);
+    //out.close();
     //std::cout << "Compiled to: " << outputPath << "\n";
     return 0;
 }
